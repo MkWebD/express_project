cae66a30e795ad7cf42d30af2c86968457c35a88
Kevin Mas <kmas@thess-corp.fr>
2023-01-13 07:12:04 +0100

    feat: parameter route with error 404 handling

--- server.js
+++ server.js
@@ -4,8 +4,29 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-	res.send("Hello World");
+const friends = [
+	{
+		id: 0,
+		name: "Albert Einstein",
+	},
+	{
+		id: 1,
+		name: "Sir Isaac Newton",
+	},
+];
+
+app.get("/friends", (req, res) => {
+	res.json(friends);
+});
+
+app.get("/friends/:friendId", (req, res) => {
+	const friendId = Number(req.params.friendId);
+	const friend = friends[friendId];
+	if (friend) {
+		res.status(200).json(friend);
+	} else {
+		res.status(404).json({ error: "Friend not found" });
+	}
 });
 
 app.get("/messages", (req, res) => {
