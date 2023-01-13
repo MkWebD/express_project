389da0b5dd7584fcd366bb4be4c1b3edd6c059b5
Kevin Mas <kmas@thess-corp.fr>
2023-01-13 16:39:37 +0100

    feat: serving html file & path defined

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,5 +1,8 @@
+const path = require("path");
+
 function getMessages(req, res) {
-	res.send("<ul><li>Hello Albert!</li></ul>");
+	path.join(__dirname, "..", "public", "skimountain.jpg");
+	res.sendFile(path.join(__dirname, "..", "public", "images", "skimountain.jpg"));
 }
 
 function postMessage(req, res) {
